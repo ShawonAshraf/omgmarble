e7f4175630845a0e49a347deb9a5cd574339b056
Shawon Ashraf <shawon13@live.com>
2019-03-02 01:12:21 +0600

    created Ball.swift

---
+++ OMGMarbles/Model/Ball.swift
@@ -0,0 +1,13 @@
+//
+//  Ball.swift
+//  OMGMarbles
+//
+//  Created by Shawon Ashraf on 3/2/19.
+//  Copyright © 2019 Shawon Ashraf. All rights reserved.
+//
+
+import SpriteKit
+
+class Ball : SKNode {
+    
+}
