b096dbc6fab5d8e25d00d5cd5cdac94df7186f3c
Shawon Ashraf <shawon13@live.com>
2019-03-02 01:32:51 +0600

    movement bounds for the ball

--- OMGMarbles/GameScene.swift
+++ OMGMarbles/GameScene.swift
@@ -11,6 +11,35 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var balls = ["ballBlue", "ballGreen", "ballPurple", "ballRed", "ballYellow"]
+    
+    override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "chekerboard")
+        
+        // puts at the center of the screen
+        background.position = CGPoint(x: frame.midX, y: frame.midY)
+        // matches the color to the background
+        background.alpha = 0.2
+        // put this behind everything so that it doesn't take over the balls
+        background.zPosition = -1
+        addChild(background)
+        
+        let ball = SKSpriteNode(imageNamed: "ballBlue")
+        let ballRadius = ball.frame.width / 2.0
+        
+        // bounds for ball movement so that it stays inside the scene
+        for i in stride(from: ballRadius, to: view.bounds.width - ballRadius, by: ball.frame.width) {
+            for j in stride(from: 100, to: view.bounds.height, by: ball.frame.height) {
+                // get a random ball
+                let ballType = balls.randomElement()!
+                let ball = Ball(imageNamed: ballType)
+                ball.position = CGPoint(x: i, y: j)
+                ball.name = ballType
+                addChild(ball)
+            }
+        }
+    }
+    
     override func sceneDidLoad() {
     }
 }
